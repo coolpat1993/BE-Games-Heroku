a428581b01239ecf98f6698e6b2a74a4b1a201a0
Patrick Shepherd <patrickshepherd218@gmail.com>
2022-09-07 15:07:25 +0100

    implemented api/reviews with query handling for categories & order

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -3,7 +3,6 @@ const app = require("../app");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
-const { string } = require("pg-format");
 
 beforeEach(() => {
   return seed(data);

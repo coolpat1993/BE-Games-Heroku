157375ea39e9688bb9ed5eebcbdff1cd723f0708
Patrick Shepherd <patrickshepherd218@gmail.com>
2022-09-30 16:07:40 +0100

    made changes to css

--- db/data/development-data/comments.js
+++ db/data/development-data/comments.js
@@ -425,5 +425,354 @@ module.exports = [
     author: 'weegembump',
     review_id: 1,
     created_at: new Date(1616854536110),
+  },{
+    body: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
+    votes: 90,
+    author: "tickle122",
+    review_id: 65,
+    created_at: new Date(788198400)
   },
+  {
+    body: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo.",
+    votes: 85,
+    author: "cooljmessy",
+    review_id: 65,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+    votes: 97,
+    author: "happyamy2016",
+    review_id: 29,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo.",
+    votes: 41,
+    author: "jessjelly",
+    review_id: 39,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.",
+    votes: 49,
+    author: "cooljmessy",
+    review_id: 36,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum.",
+    votes: 15,
+    author: "jessjelly",
+    review_id: 43,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus.",
+    votes: 4,
+    author: "cooljmessy",
+    review_id: 29,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+    votes: 45,
+    author: "happyamy2016",
+    review_id: 57,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+    votes: 74,
+    author: "happyamy2016",
+    review_id: 30,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Mauris ullamcorper purus sit amet nulla.",
+    votes: 5,
+    author: "jessjelly",
+    review_id: 56,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Aenean sit amet justo.",
+    votes: 96,
+    author: "happyamy2016",
+    review_id: 50,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Pellentesque ultrices mattis odio. Donec vitae nisi.",
+    votes: 20,
+    author: "weegembump",
+    review_id: 59,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    votes: 89,
+    author: "happyamy2016",
+    review_id: 51,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    votes: 35,
+    author: "jessjelly",
+    review_id: 47,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",
+    votes: 55,
+    author: "jessjelly",
+    review_id: 40,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus.",
+    votes: 92,
+    author: "jessjelly",
+    review_id: 63,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    votes: 91,
+    author: "weegembump",
+    review_id: 39,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    votes: 41,
+    author: "jessjelly",
+    review_id: 32,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+    votes: 62,
+    author: "happyamy2016",
+    review_id: 44,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.",
+    votes: 91,
+    author: "weegembump",
+    review_id: 52,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+    votes: 21,
+    author: "jessjelly",
+    review_id: 61,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.",
+    votes: 13,
+    author: "jessjelly",
+    review_id: 31,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo.",
+    votes: 62,
+    author: "cooljmessy",
+    review_id: 60,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+    votes: 96,
+    author: "weegembump",
+    review_id: 28,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+    votes: 36,
+    author: "cooljmessy",
+    review_id: 27,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus.",
+    votes: 84,
+    author: "tickle122",
+    review_id: 49,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.",
+    votes: 8,
+    author: "happyamy2016",
+    review_id: 45,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Aliquam erat volutpat.",
+    votes: 83,
+    author: "happyamy2016",
+    review_id: 53,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    votes: 28,
+    author: "weegembump",
+    review_id: 32,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet.",
+    votes: 10,
+    author: "tickle122",
+    review_id: 39,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    votes: 6,
+    author: "tickle122",
+    review_id: 59,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Nullam molestie nibh in lectus. Pellentesque at nulla.",
+    votes: 8,
+    author: "happyamy2016",
+    review_id: 32,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    votes: 35,
+    author: "tickle122",
+    review_id: 47,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Donec ut mauris eget massa tempor convallis.",
+    votes: 14,
+    author: "jessjelly",
+    review_id: 37,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    votes: 22,
+    author: "happyamy2016",
+    review_id: 52,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.",
+    votes: 87,
+    author: "tickle122",
+    review_id: 52,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Duis ac nibh.",
+    votes: 3,
+    author: "jessjelly",
+    review_id: 48,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus.",
+    votes: 60,
+    author: "jessjelly",
+    review_id: 54,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Cras non velit nec nisi vulputate nonummy.",
+    votes: 35,
+    author: "weegembump",
+    review_id: 50,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum.",
+    votes: 21,
+    author: "tickle122",
+    review_id: 43,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+    votes: 53,
+    author: "jessjelly",
+    review_id: 31,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",
+    votes: 83,
+    author: "grumpy19",
+    review_id: 41,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
+    votes: 70,
+    author: "tickle122",
+    review_id: 48,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Donec vitae nisi.",
+    votes: 72,
+    author: "cooljmessy",
+    review_id: 41,
+    created_at: new Date(1610964545610)
+  },
+  {
+    body: "Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+    votes: 79,
+    author: "tickle122",
+    review_id: 46,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.",
+    votes: 24,
+    author: "grumpy19",
+    review_id: 33,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti.",
+    votes: 64,
+    author: "tickle122",
+    review_id: 46,
+    created_at: new Date(788198400)
+  },
+  {
+    body: "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla.",
+    votes: 90,
+    author: "grumpy19",
+    review_id: 57,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",
+    votes: 84,
+    author: "weegembump",
+    review_id: 26,
+    created_at: new Date(1610964545410)
+  },
+  {
+    body: "Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.",
+    votes: 48,
+    author: "happyamy2016",
+    review_id: 30,
+    created_at: new Date(788198400)
+  }
 ];
